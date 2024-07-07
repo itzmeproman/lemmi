249475b6aa88b683d6762b4e47ef649e60118672
itzmeproman <atulpandey3030@gmail.com>
2024-07-07 09:20:33 +0530

    Add files via upload

---
+++ Config public.py
@@ -0,0 +1,163 @@
+BOT_TOKEN = "6936427405:AAFzNCPGQJD1AmTUM06-YuejKvqTrkCArYA"                              # Require restart after changing it while bot running
+OWNER_ID = "7030439873"                               # Require restart after changing it while bot running
+TELEGRAM_API = "21310924"                           # Require restart after changing it while bot running
+TELEGRAM_HASH = "fa4c3f582286d969ab1d08449e9533e8"                          # Require restart after changing it while bot running
+
+# OPTIONAL CONFIG
+USER_SESSION_STRING = ""                    # Require restart after changing it while bot running
+DATABASE_URL = "mongodb+srv://sai:sai@cluster0.ayo8gc4.mongodb.net/?retryWrites=true&w=majority"                           # Require restart after changing it while bot running
+DOWNLOAD_DIR = "/usr/src/app/downloads/"    # Require restart after changing it while bot running
+CMD_SUFFIX = ""                             # Require restart after changing it while bot running
+AUTHORIZED_CHATS = "-1001985920600"                       # Require restart after changing it while bot running
+SUDO_USERS = ""                             # Require restart after changing it while bot running
+BLACKLIST_USERS = ""                        # Require restart after changing it while bot running
+STATUS_LIMIT = "4"
+DEFAULT_UPLOAD = "gd"
+STATUS_UPDATE_INTERVAL = "10"
+AUTO_DELETE_MESSAGE_DURATION = "60"
+INCOMPLETE_TASK_NOTIFIER = "False"
+SET_COMMANDS = "False"
+EXTENSION_FILTER = ""
+YT_DLP_OPTIONS = ""
+FSUB_IDS = "-1001926897432"
+BOT_PM = "True"
+
+# GDrive Tools
+GDRIVE_ID = ""
+USER_TD_MODE = ""
+USER_TD_SA = ""
+INDEX_URL = ""
+USE_SERVICE_ACCOUNTS = "False"
+IS_TEAM_DRIVE = "False"
+STOP_DUPLICATE = "False"
+DISABLE_DRIVE_LINK = "False"
+GD_INFO = "Uploaded by WZML-X"
+
+# API's/Cookies
+REAL_DEBRID_API = ""
+DEBRID_LINK_API = ""
+FILELION_API = ""
+GDTOT_CRYPT = ""
+JIODRIVE_TOKEN = ""
+
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = ""
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+
+# Update
+UPSTREAM_REPO = "https://github.com/Itzmeproman/WZML-X"
+UPSTREAM_BRANCH = "hk_wzmlx"
+UPGRADE_PACKAGES = ""
+
+# Leech & Mirror
+LEECH_SPLIT_SIZE = ""
+AS_DOCUMENT = "False"
+EQUAL_SPLITS = "False"
+MEDIA_GROUP = "False"
+CAP_FONT = "code"
+LEECH_FILENAME_PREFIX = ""
+LEECH_FILENAME_SUFFIX = ""
+LEECH_FILENAME_CAPTION = ""
+LEECH_FILENAME_REMNAME = ""
+MIRROR_FILENAME_PREFIX = ""
+MIRROR_FILENAME_SUFFIX = ""
+MIRROR_FILENAME_REMNAME = ""
+
+# Log Channel/SuperGroup (Topics Support)
+LEECH_LOG_ID = "-1001936525973"
+MIRROR_LOG_ID = ""
+LINKS_LOG_ID = "-1001936525973"
+
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = ""
+BASE_URL = ""
+BASE_URL_PORT = ""
+WEB_PINCODE = "False"
+
+#Queueing system
+QUEUE_ALL = ""
+QUEUE_DOWNLOAD = ""
+QUEUE_UPLOAD = ""
+
+# RSS
+RSS_DELAY = "600"
+RSS_CHAT = ""
+
+# Mega
+MEGA_EMAIL = ""
+MEGA_PASSWORD = ""
+
+# Limits
+DAILY_TASK_LIMIT = ""
+DAILY_MIRROR_LIMIT = ""
+DAILY_LEECH_LIMIT = ""
+USER_MAX_TASKS = ""
+BOT_MAX_TASKS = ""
+TORRENT_LIMIT= ""
+DIRECT_LIMIT = ""
+GDRIVE_LIMIT = ""
+CLONE_LIMIT = ""
+YTDLP_LIMIT = ""
+PLAYLIST_LIMIT = ""
+LEECH_LIMIT = ""
+MEGA_LIMIT = ""
+STORAGE_THRESHOLD = ""
+USER_TIME_INTERVAL = "0"
+
+# Templates
+ANIME_TEMPLATE = ""
+IMDB_TEMPLATE = ""
+MDL_TEMPLATE = ""
+
+# Telegraph
+TITLE_NAME = "WZ-M/L-X"
+AUTHOR_NAME = "WZML-X"
+AUTHOR_URL = "https://t.me/Anime_Compass"
+COVER_IMAGE = ""
+
+# Extra
+SAFE_MODE = ""
+DELETE_LINKS = ""
+CLEAN_LOG_MSG = ""
+SHOW_EXTRA_CMDS = ""
+TIMEZONE = "Asia/Kolkata"
+IMAGES = ""
+IMG_SEARCH = ""
+IMG_PAGE = ""
+BOT_THEME = "minimal"
+EXCEP_CHATS = ""
+
+# M/L Buttons
+SCREENSHOTS_MODE = "False"
+SHOW_MEDIAINFO = "True"
+SAVE_MSG = "True"
+SOURCE_LINK = "True"
+
+# Token system
+TOKEN_TIMEOUT = "43200"
+LOGIN_PASS = ""
+
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = ""
+SEARCH_PLUGINS = '["https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+                   "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+                   "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+                   "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+                   "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py"]'
